5bea883dcf6353ecbe5f6d3c7a6d72f331a9345c
bkienzle3 <brian@bkienzle.com>
2024-12-28 16:05:38 -0500

    proof of concept

--- gx-converter/main.py
+++ gx-converter/main.py
@@ -1,4 +1,26 @@
+from g_code_property_extractor import GCodePropertyExtractor
+from g_code_transformer import GCodeTransformer
 from header import Header
 
-a = Header(1, 1, 1, 1, 1, 1, 222)
-pass
+
+with open("../scratch/calibration-cube.gcode", "r") as f:
+    code = f.read()
+
+hotbed_temp = GCodePropertyExtractor.get_hotbed_temperature(code)
+nozzle_temp = GCodePropertyExtractor.get_nozzle_temperature(code)
+print_time = GCodePropertyExtractor.get_print_time(code)
+filament_usage = GCodePropertyExtractor.get_filament_usage(code)
+layer_height = GCodePropertyExtractor.get_layer_height(code)
+shell_count = 3
+print_speed = 60
+
+code = GCodeTransformer.convert(code)
+
+header = Header(
+    print_time, filament_usage, layer_height, 1, print_speed, hotbed_temp, nozzle_temp
+)
+
+gx = header.assembled_header + code.encode("utf-8")
+
+with open("../scratch/cube-test.gx", "wb") as f:
+    f.write(gx)
